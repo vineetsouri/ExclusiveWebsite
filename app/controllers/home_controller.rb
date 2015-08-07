46ff77edaad0b4b423c59407b7198815c15f82ed
vineetsouri <vineetsouri72@gmail.com>
2015-08-08 01:50:07 +0530

    show timer on ticket page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
 class HomeController < ApplicationController
   def welcome
-    if User.any?
-      token_time = User.first.created_at + User.count.minutes
-      @seconds_left = (token_time - Time.now).to_i
+    if current_ticket
+      @waiting_count = Ticket.waiting.count
+      @current_ticket = current_ticket.ticket
+      @new_ticket = Ticket.new
+      @new_ticket.ticket_no = Ticket.count + 1
+      @seconds_left = CurrentTicket.seconds_left
     else
       @seconds_left = 0
     end
-    @user = User.new
-  end
-
-  def waiting
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807081656) do
+ActiveRecord::Schema.define(version: 20150807125600) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "current_tickets", force: :cascade do |t|
+    t.integer  "ticket_id"
+    t.datetime "activated_at"
+  end
+
+  add_index "current_tickets", ["ticket_id"], name: "index_current_tickets_on_ticket_id"
+
+  create_table "tickets", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "ticket_no"
+    t.boolean  "waiting",    default: true
   end
 
 end
