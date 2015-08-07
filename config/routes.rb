68f0313563808b21ece1579b21b73a5a83919f43
vineetsouri <vineetsouri72@gmail.com>
2015-08-07 16:56:26 +0530

    added flipclock and stylings with dummy timer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'waiting' => 'home#waiting', as: :waiting
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
