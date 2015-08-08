034e8abad6e0fbef268afd383f0018e238a9168a
vineetsouri <vineetsouri72@gmail.com>
2015-08-08 14:23:46 +0530

    Added restriction to users with out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_ticket
-  
+
   def current_ticket
     @current_ticket ||= CurrentTicket.fetch
+  end
+
+  def deny_access!
+    redirect_to root_path, alert: "Access Denied!"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807125600) do
+ActiveRecord::Schema.define(version: 20150808084915) do
 
   create_table "current_tickets", force: :cascade do |t|
     t.integer  "ticket_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150807125600) do
     t.datetime "updated_at",                null: false
     t.integer  "ticket_no"
     t.boolean  "waiting",    default: true
+    t.string   "code"
   end
 
 end
