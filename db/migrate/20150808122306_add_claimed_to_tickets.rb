120e5c9d2a73b7ae4f66cc49648b82a9ea7f1efd
vineetsouri <vineetsouri72@gmail.com>
2015-08-08 18:00:38 +0530

    made the deafult value for claimed tickets as false

--- db/migrate/20150808122306_add_claimed_to_tickets.rb
+++ db/migrate/20150808122306_add_claimed_to_tickets.rb
@@ -1,5 +1,5 @@
 class AddClaimedToTickets < ActiveRecord::Migration
   def change
-    add_column :tickets, :claimed, :boolean
+    add_column :tickets, :claimed, :boolean, default: false
   end
 end
